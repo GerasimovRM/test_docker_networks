57367232d10ffda113686d70796b9bf17f42d6df
Roman M. Gerasimov <romagrizly@gmail.com>
2023-09-15 20:04:03 +0500

    docker-compose test

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
-cd server1
-docker build -t test_server_1
-cd ../server2
+docker compose build
+docker compose up -d
+cd server2
 docker build -t test_server_2
-docker run -itd --network=test_bridge -p 5500:5500 --name test_server_1 test_server_1 python3 main.py
 docker run -itd --network=test_bridge -p 5501:5500 --name test_server_2 test_server_2 python3 main.py
